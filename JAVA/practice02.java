676a2f393ac6a29a045bb8826682e2ac8aeee130
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-04-25 13:12:11 +0900

    Create practice02.java

---
+++ JAVA/practice02.java
@@ -0,0 +1,13 @@
+switch (fortune) {
+    case  1:
+      System.out.println("大吉");
+      break;
+    case 2:
+      System.out.println("中吉");
+      break;
+    case 3:
+      System.out.println("吉");
+      break;
+  default:
+    System.out.println("");      
+}
