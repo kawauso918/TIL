5c18f60df2d93e179d8b02826aba11edabd45c5c
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-04-26 14:02:08 +0900

    Create practice10.java
    メソッド呼び出し

---
+++ JAVA/practice10.java
@@ -0,0 +1,13 @@
+package javaWcp;
+
+public class Practice75 {
+	public static void main(String[] args) {
+		System.out.println("メソッド呼び出し");
+		hellow();
+		System.out.println("メソッド呼び出し終了");
+	}
+	public static void hellow() {
+		System.out.println("こんにちは");
+	}
+
+}
