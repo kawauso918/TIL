45142f8d3ca44214a12731a343d9b55f494166c6
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-04-26 13:28:21 +0900

    Create practice08.java
    50

---
+++ JAVA/practice08.java
@@ -0,0 +1,12 @@
+package javaWcp;
+
+public class Practice73 {
+	public static void main (String[] args) {
+		int [] [] scores = new int[2][3];
+		scores[0][0] = 40;
+		scores[0] [1] = 50;
+		scores[0] [2] = 60;
+		System.out.println(scores[0][1]);
+	}
+
+}
