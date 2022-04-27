375784d3d23a8b8a99f8645e8cc3f16088d27bec
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-04-27 13:08:33 +0900

    Create practice12.java

---
+++ JAVA/practice12.java
@@ -0,0 +1,12 @@
+package javaWcp;
+
+public class Practice77 {
+	public static void main (String[] args) {
+		introduceOneself();
+	}
+	public static void introduceOneself() {
+		String name = "minami";
+		System.out.println("わたしの名前は"+ name);
+	}
+
+}
