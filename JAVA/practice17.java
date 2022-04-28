a58b50cda33aabffaef97f91bbbde4a9c42307af
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-04-28 13:37:33 +0900

    Create practice17.java

---
+++ JAVA/practice17.java
@@ -0,0 +1,12 @@
+package javaWcp;
+
+public class Practice79 {
+	public static void main(String[]args) {
+		Hero h = new Hero();
+		h.name = "ミナト";
+		h.hp = 100;
+		System.out.println("勇者"+ h.name + "生み出し");
+		
+		h.sit(5);
+	}
+}
