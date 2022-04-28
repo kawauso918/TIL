3ab81cea66aeb5a0bab3a55e0c6a1f5be4c588d2
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-04-28 13:25:23 +0900

    Update practice16.java

--- JAVA/practice16.java
+++ JAVA/practice16.java
@@ -5,6 +5,12 @@ public class Hero {
 	int hp;
 	public void sleep() {
 		this.hp = 100;
-		System.out.println(this.name +"眠って回復");
+	System.out.println(this.name +"眠って回復");
+	}
+	public void sit(int sec) {
+		this.hp += sec;
+		System.out.println(this.name +"は、"+ "秒座った");
+		System.out.println("HP"+ sec+ "ポイント回復");
+		
 	}
 }
