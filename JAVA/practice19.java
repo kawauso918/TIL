dca6aba8a41d4a101956fde87322e6bdcecca199
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-04-28 14:03:07 +0900

    Create practice19.java

---
+++ JAVA/practice19.java
@@ -0,0 +1,16 @@
+package javaWcp;
+
+public class Practice79 {
+	public static void main(String[]args) {
+		Hero h = new Hero();
+		h.name = "ミナト";
+		h.hp = 100;
+		
+		Matango m1 = new Matango();
+		m1.hp = 50;
+		m1.suffix = 'A';
+		
+		
+		m1.run();
+	}
+}
