377b5bfc84dc264d3c03bfd336bd037a84251a49
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-05-02 13:59:45 +0900

    Create practice26.java

---
+++ JAVA/practice26.java
@@ -0,0 +1,11 @@
+package javaWcp;
+
+public class Practice83 {
+	public static void main(String[]args) {
+		Hero h1 = new Hero("ミナト");
+		System.out.println(h1.name);
+		Hero h2 = new Hero();
+		System.out.println(h2.name);
+	}
+
+}
