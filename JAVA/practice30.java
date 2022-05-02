782b30c1ad8d845c7938758c22cbecb5e6ac7f2c
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-05-02 14:34:13 +0900

    Create practice30.java

---
+++ JAVA/practice30.java
@@ -0,0 +1,17 @@
+package javaWcp;
+
+public class SuperHero extends Hero{
+	boolean flying;
+	public void fly() {
+		this.flying = true;
+		System.out.println("飛び上がった");
+	}
+	public void land() {
+		this.flying = false;
+		System.out.println("着地した");
+	}
+	public void run() {
+		System.out.println(this.name +"逃げ出した");
+	}
+
+}
