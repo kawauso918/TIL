3daab9bd435fc8313a9678701f63086320e2cf9b
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-05-03 15:09:38 +0900

    Create practice33.java

---
+++ JAVA/practice33.java
@@ -0,0 +1,19 @@
+package javaWcp;
+
+public class PoisonMatango extends Matango {
+	int poisonCount =5;
+	public PoisonMatango(char suffix) {
+		super(suffix);
+	}
+	public void attack (Hero h) {
+		super.attack(h);
+		if (this.poisonCount > 0) {
+			System.out.println("毒をばらまいた");
+			int dmg = h.hp /5;
+			h.hp -= dmg;
+			System.out.println(dmg + "ポイントのダメージ");
+			this.poisonCount--;
+		}
+	}
+
+}
