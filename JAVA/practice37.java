036c54434620f990339c16adfdd2ff66b81c4ea8
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-05-05 17:37:47 +0900

    Create practice37.java

---
+++ JAVA/practice37.java
@@ -0,0 +1,11 @@
+package javaWcp;
+
+public class Practice87 {
+	public static void main (String[]args) {
+		Hero h = new Hero();
+		h.name = "ミナミ";
+		h.hp = 100;
+		System.out.println("勇者"+ h.name+ "生み出した");
+	}
+
+}
