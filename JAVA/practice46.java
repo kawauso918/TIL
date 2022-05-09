70629135aa404d831c4b4c1629e1a064d1a57a6e
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-05-09 17:32:40 +0900

    Create practice46.java
    コンストラクタ

---
+++ JAVA/practice46.java
@@ -0,0 +1,9 @@
+package javaWcp;
+
+public class Practice88 {
+	public static void main (String[]args) {
+		Hero h = new Hero("ミナト");
+		System.out.println(h.hp);
+		System.out.println(h.name);
+	}
+}
