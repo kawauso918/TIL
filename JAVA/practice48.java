44e104a4d114c16395d9edd035995ef0f73604c4
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-05-10 13:39:13 +0900

    Create practice48.java
    コンストラクタ①を呼び出す

---
+++ JAVA/practice48.java
@@ -0,0 +1,19 @@
+package javaWcp;
+
+public class Hero {
+	String name;
+	int hp;
+	Sword sword;
+	public void attack() {
+		System.out.println(this.name+"攻撃");
+		System.out.println("ダメージを与えた");
+	}
+	public Hero(String name) {
+		this.hp =100;
+		this.name = name;
+	}
+	public Hero() {
+		this("ダミー");
+	}
+}
+
