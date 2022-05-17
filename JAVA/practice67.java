fb795dd735224936828d3cd7b486c5d20007ff4b
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-05-17 13:18:18 +0900

    Create practice67.java

---
+++ JAVA/practice67.java
@@ -0,0 +1,11 @@
+package javaWcp;
+
+public class Practice91 {
+	public static void main(String[]args) {
+		Character[] c = new Character[5];
+		c[0] = new Hero();
+		c[1] = new Hero();
+		c[2] = new Thief();
+	}
+
+}
