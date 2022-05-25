53c7db495de39b5b2e58ae7faf1acc4e47412119
kawauso918 <93640642+kawauso918@users.noreply.github.com>
2022-05-25 10:27:44 +0900

    Create practice79.java

---
+++ JAVA/practice79.java
@@ -0,0 +1,12 @@
+package javaWcp;
+
+public class Practice93 {
+	public static void main(String[]args) {
+		Wizard w = new Wizard();
+		Matango m = new Matango();
+		w.name = "アサカ";
+		w.attack(m);
+		w.fireball(m);
+	}
+
+}
